099de9af155c8d15d52f0910279270cbe01b136c
HLRichardson-Git <101138530+HLRichardson-Git@users.noreply.github.com>
2022-10-24 21:56:28 -0500

    Create basic_password_zk.py

---
+++ basic_password_zk.py
@@ -0,0 +1,26 @@
+import random
+import math
+
+r = random.randrange(0, 500)
+
+x = input('Enter your x: ')
+x = int(x)
+g = 3
+p = 5
+
+c = (g**r) % p
+print('C is: ' + str(c))
+
+w = (x + r) % (p - 1)
+
+print('W is: ' + str(w))
+
+y =(g**x)%p
+
+test = (y*c)%p
+test2 = (g**w)%p
+
+if test == test2:
+    print('wow')
+else:
+    print('fuck math')
